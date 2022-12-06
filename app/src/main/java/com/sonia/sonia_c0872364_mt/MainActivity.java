f785977ebc55bafff7b73ac5c1880d8358121d6a
Sonia364 <c0872364@mylambton.ca>
2022-12-06 01:02:24 +0000

    Added the seekbar functionality and displayed the resulted number in textview

--- app/src/main/java/com/sonia/sonia_c0872364_mt/MainActivity.java
+++ app/src/main/java/com/sonia/sonia_c0872364_mt/MainActivity.java
@@ -7,12 +7,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener , View.OnClickListener {
 
     Spinner spinner;
     EditText price;
+    SeekBar seekbar;
+    int days = 1;
+    TextView daysTextVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         spinner = findViewById(R.id.carsList);
         price = findViewById(R.id.carPrice);
+        seekbar = findViewById(R.id.daysSeekbar);
+        daysTextVal = findViewById(R.id.daysText);
 
         spinner.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.cars, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                days = progressChangedValue;
+                daysTextVal.setText(String.valueOf(days));
+//                Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+//                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
