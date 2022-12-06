a52c6887ab4bfddb108d49096deef3b69f3e43c1
Sonia364 <c0872364@mylambton.ca>
2022-12-06 00:47:10 +0000

    Linked cars adapter to spinner

--- app/src/main/java/com/sonia/sonia_c0872364_mt/MainActivity.java
+++ app/src/main/java/com/sonia/sonia_c0872364_mt/MainActivity.java
@@ -3,12 +3,22 @@ package com.sonia.sonia_c0872364_mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.carsList);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cars, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
